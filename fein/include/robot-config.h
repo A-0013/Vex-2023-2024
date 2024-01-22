236d59445fd32f817f8042e380221745c473be0a
A-0013 <157261625+A-0013@users.noreply.github.com>
2024-01-21 19:43:34 -0500

    Add files via upload

---
+++ fein/include/robot-config.h
@@ -0,0 +1,22 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern digital_out Wings1;
+extern digital_out Wings2;
+extern motor_group Lift;
+extern motor Intake;
+extern digital_out Hanger;
+extern motor_group LDrive_Group;
+extern motor_group RDrive_Group;
+extern gps GPS8;
+extern motor shooter;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
